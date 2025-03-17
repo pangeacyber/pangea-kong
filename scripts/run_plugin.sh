d3e77cba6ef118f4ce5e2b6c9f55e011e4ad80f5
H. Andres Tournour <andres.tournour@pangea.cloud>
2025-03-17 14:51:29 -0300

    fixup! scripts updates

--- scripts/run_plugin.sh
+++ scripts/run_plugin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-$CONTAINER_NAME = "pangea-kong-container"
+CONTAINER_NAME = "pangea-kong-container"
 
-docker kill $CONTAINER_NAME
-docker container rm $CONTAINER_NAME
+docker kill "$CONTAINER_NAME"
+docker container rm "$CONTAINER_NAME"
 
 docker run -d -it --network=kong-net -p 8010:8000 -p 8011:8001 -p 8012:8002 \
 -e KONG_ADMIN_LISTEN=0.0.0.0:8001 \
@@ -11,5 +11,5 @@ docker run -d -it --network=kong-net -p 8010:8000 -p 8011:8001 -p 8012:8002 \
 -e KONG_PG_HOST=kong-postgres \
 -e KONG_PG_USER=kong \
 -e KONG_PG_PASSWORD=kongpass \
---name $CONTAINER_NAME \
+--name "$CONTAINER_NAME" \
 pangea_kong_plugin
--- scripts/setup_database.sh
+++ scripts/setup_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$CONTAINER_NAME = "pangea-kong-container"
+CONTAINER_NAME = "pangea-kong-container"
 
-docker kill $CONTAINER_NAME
-docker container rm $CONTAINER_NAME
+docker kill "$CONTAINER_NAME"
+docker container rm "$CONTAINER_NAME"
 
 docker run -d -it --network=kong-net -p 8010:8000 -p 8011:8001 -p 8012:8002 \
 -e KONG_ADMIN_LISTEN=0.0.0.0:8001 \
@@ -10,5 +10,5 @@ docker run -d -it --network=kong-net -p 8010:8000 -p 8011:8001 -p 8012:8002 \
 -e KONG_PG_HOST=kong-postgres \
 -e KONG_PG_USER=kong \
 -e KONG_PG_PASSWORD=kongpass \
---name $CONTAINER_NAME \
+--name "$CONTAINER_NAME" \
 pangea_kong_plugin kong migrations bootstrap
