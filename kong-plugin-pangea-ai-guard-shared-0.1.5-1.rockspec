341e972b2ef8090400c03025a79b5c0424c10110
Konstantin Lapine <konstantin.lapine@pangea.cloud>
2025-06-01 14:05:22 -0700

    chore: Bump patch version to 0.1.5

--- kong-plugin-pangea-ai-guard-shared-0.1.5-1.rockspec
+++ kong-plugin-pangea-ai-guard-shared-0.1.5-1.rockspec
@@ -1,4 +1,4 @@
-local package_version = "0.1.4"
+local package_version = "0.1.5"
 local rockspec_revision = "1"
 
 package = "kong-plugin-pangea-ai-guard-shared"
--- kong/plugins/pangea-ai-guard-request/handler.lua
+++ kong/plugins/pangea-ai-guard-request/handler.lua
@@ -5,7 +5,7 @@ local kong_utils = require("kong.tools.gzip")
 local PangeaAIGuardRequestHandler = {
 	-- Need to run BEFORE ai-proxy
 	PRIORITY = 780,
-	VERSION = "0.1.4",
+	VERSION = "0.1.5",
 }
 
 local function get_raw_body()
